79b024c6cd5c2bd2d0bf04d7feff342fb94e1691
harshitagoyalcs28-tech <harshita.goyal.cs28@iilm.edu>
2026-02-28 09:44:35 +0530

    Update exercise 3.sql

--- exercise 3.sql
+++ exercise 3.sql
@@ -1,1 +1,14 @@
 1.Find all the Toy Story movies
+SELECT title, director FROM movies 
+WHERE title LIKE "Toy Story%";
+2.Find all the movies directed by John Lasseter
+SELECT title, director FROM movies 
+WHERE director = "John Lasseter";
+3.Find all the movies (and director) not directed by John Lasseter
+SELECT title, director FROM movies 
+WHERE director = "John Lasseter";
+4.Find all the WALL-* movies
+SELECT * FROM movies 
+WHERE title LIKE "WALL-_";
+
+
