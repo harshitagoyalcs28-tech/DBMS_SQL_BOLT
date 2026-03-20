fbf387c7a262f5109ea9c7a1baa3c38742a71e35
harshitagoyalcs28-tech <harshita.goyal.cs28@iilm.edu>
2026-03-20 13:20:15 +0530

    Add files via upload

---
+++ exercise 9.sql
@@ -0,0 +1,11 @@
+1.List all movies and their combined sales in millions of dollars
+SELECT title, (domestic_sales + international_sales) / 1000000 AS gross_sales_millions
+FROM movies
+  JOIN boxoffice
+    ON movies.id = boxoffice.movie_id;
+2.List all movies and their ratings in percent
+SELECT title, rating * 10 AS rating_percent
+FROM movies
+  JOIN boxoffice
+    ON movies.id = boxoffice.movie_id;
+3.        
